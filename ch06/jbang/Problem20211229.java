4e36b05176803d9b753f984a8e9e186660586195
jabrena <bren@juanantonio.info>
2021-12-29 10:24:37 +0000

    Refactoring solution to use BigDecimal

--- ch06/jbang/Problem20211229.java
+++ ch06/jbang/Problem20211229.java
@@ -9,6 +9,7 @@ import java.util.function.Function;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 
 public class Problem20211229 {
 
@@ -20,14 +21,14 @@ public class Problem20211229 {
                 .reduce((n1, n2) -> n1.multiply(n2))
                 .get();
 
-        var precision = 20;
+        var precision = 50;
         var roundingMode = RoundingMode.HALF_UP;
-        
+
         var component1 = factorial.apply(11L).divide(factorial.apply(22L), precision, roundingMode);
         var component2 = factorial.apply(21L).divide(factorial.apply(10L), precision, roundingMode);
         var component3 = BigDecimal.valueOf(58);
-        var result = component1.multiply(component2).multiply(component3).setScale(0, roundingMode);
+        var result = component1.multiply(component2).multiply(component3);
 
-        assertThat(result).as("Result is: ").isEqualTo(BigDecimal.valueOf(29));
+        assertThat(result.doubleValue()).as("Result is: ").isEqualTo(29);
     }
 }
