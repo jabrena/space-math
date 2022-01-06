cd773231857f52a7a5d20f009e805476764c4fb9
jabrena <bren@juanantonio.info>
2022-01-06 16:26:20 +0000

    Improving solution

--- ch06/jbang/dailyepsilon/Problem20220106.java
+++ ch06/jbang/dailyepsilon/Problem20220106.java
@@ -3,26 +3,28 @@
 
 package ch06.jbang.dailyepsilon;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 
 public class Problem20220106 {
 
     public static void main(String[] args) {
 
-        var iterations = 100000;
-        var roundingMode = RoundingMode.HALF_UP;
+        var iterations = 10_0000_000;
 
-        var op1 = BigDecimal.valueOf(Math.PI).pow(2);
-        var op2 = IntStream.rangeClosed(1, iterations).boxed()
-            .map(BigDecimal::valueOf)
-            .map(n -> BigDecimal.ONE.divide(n.pow(2), roundingMode))
-            .reduce(BigDecimal.ZERO, BigDecimal::add);
+        //Sum(1/x^2)=Pi^2/6
+        var op1 = IntStream.rangeClosed(1, iterations).boxed()
+            .map(n -> 1 / Math.pow(n, 2))
+            .collect(Collectors.summingDouble(Double::doubleValue));
+        var op2 = (Math.PI * Math.PI) / 6;
 
-        var result = op1.divide(op2, roundingMode);
-
+        System.out.println(op1);
         System.out.println(op2);
+        
+        assertThat(op1).isCloseTo(op2, within(Double.valueOf("1E-8")));
     }
 }
 
