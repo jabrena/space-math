4be37c5750b2dcbf887a79ab6eaecb049df7ae62
jabrena <bren@juanantonio.info>
2022-01-06 21:59:31 +0000

    Adding Proper solution

--- ch06/jbang/dailyepsilon/Problem20220106.java
+++ ch06/jbang/dailyepsilon/Problem20220106.java
@@ -3,6 +3,9 @@
 
 package ch06.jbang.dailyepsilon;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -13,18 +16,26 @@ public class Problem20220106 {
 
     public static void main(String[] args) {
 
-        var iterations = 10_0000_000;
+        var iterations = 1_000_000;
 
-        //Sum(1/x^2)=Pi^2/6
-        var op1 = IntStream.rangeClosed(1, iterations).boxed()
-            .map(n -> 1 / Math.pow(n, 2))
-            .collect(Collectors.summingDouble(Double::doubleValue));
-        var op2 = (Math.PI * Math.PI) / 6;
+        var roundingMode = RoundingMode.HALF_UP;
+        var precision = new MathContext(20);
+
+        var op1 = new BigDecimal(String.valueOf(Math.PI)).pow(2, precision);
+        var op2 = IntStream.rangeClosed(1, iterations).boxed()
+            .map(n -> 1 / Math.pow(n, 2))//TODO Refactor using only BigDecimal methods
+            .map(String::valueOf)
+            .map(BigDecimal::new)
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
+        var result = op1.divide(op2, roundingMode);
 
         System.out.println(op1);
         System.out.println(op2);
-        
-        assertThat(op1).isCloseTo(op2, within(Double.valueOf("1E-8")));
+        System.out.println(result);
+
+        assertThat(result)
+            .usingComparator(BigDecimal::compareTo)
+            .isCloseTo(BigDecimal.valueOf(6), within(new BigDecimal("1E-5")));
     }
 }
 
