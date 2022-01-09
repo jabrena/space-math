3842b55fd31e6db87737b2a0473ab55445adf566
jabrena <bren@juanantonio.info>
2022-01-09 09:17:10 +0000

    Improving solution

--- ch06/jbang/dailyepsilon/Problem20220109.java
+++ ch06/jbang/dailyepsilon/Problem20220109.java
@@ -8,7 +8,11 @@ import java.math.MathContext;
 import java.math.RoundingMode;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -28,17 +32,33 @@ public class Problem20220109 {
             return (counter == 20) ? true : false;
         };
 
+        Supplier<Integer> compute = () -> {
+            return Stream.iterate(1, i -> i + 1) //Infinite Stream
+                .filter(isDivisible_1_to_20)
+                .limit(1)
+                .peek(System.out::println)
+                .map(String::valueOf)
+                .map(String::length)
+                .findAny()
+                .orElseThrow();
+        };
+
+        //Protect computatation against infinite scenarios with Timeout support
+        Supplier<Integer> computeAsync = () -> {
+            return CompletableFuture
+                    .supplyAsync(() -> compute.get())
+                    .orTimeout(120, TimeUnit.SECONDS)
+                    .handle((response, ex) -> {
+                        if(!Objects.isNull(ex)) {
+                            return -99;
+                        }
+                        return response;
+                    }).join();
+        };
+
         Instant start = Instant.now();
 
-        var result = Stream.iterate(1, i -> i + 1) //Infinite Stream
-            .filter(isDivisible_1_to_20)
-            .limit(1)
-            .peek(System.out::println)
-            .map(String::valueOf)
-            .map(String::length)
-            .findAny()
-            .orElseThrow();
-
+        var result = computeAsync.get();
         System.out.println(result);
 
         Instant end = Instant.now();
